ab033247a1655cdbcaa300bd686522b8df510136
Tanya Ignatenko <tanigna.work@gmail.com>
2023-07-05 23:03:16 +0700

    Update App.tsx

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
     const [isFirstImage, setIsFirstImage] = useState(true);
     const [userImage, setUserImage] = useState<string>();
     const [result, setResult] = useState<{
-        value?: string,
+        value?: string | null,
         isProcessing?: boolean,
     }>();
 
@@ -30,6 +30,10 @@ export default function App() {
         }
 
         loadFile(files[0], (event: ProgressEvent<FileReader>) => {
+            setResult({
+                value: null,
+                isProcessing: false,
+            });
             setUserImage(event.target?.result as string);
             if (isFirstImage) setIsFirstImage(false);
         });
